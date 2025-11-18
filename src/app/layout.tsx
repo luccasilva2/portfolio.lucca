81e17af09fd0da6e1d4a6cc87dd0f35d187c3226
Lucca TI <testesti2007@gmail.com>
2025-11-18 01:57:40 +0000

    krl que merda que tu fez, tem delay pra mecher no scroll agr KKKKKKKKKK,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import { SmoothScroll } from '@/components/layout/smooth-scroll';
 
 export const metadata: Metadata = {
   title: 'Lucca Silva Oliveira - Premium Digital Portfolio',
@@ -21,9 +20,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Poppins:wght@600;700;800&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <SmoothScroll>
-          {children}
-        </SmoothScroll>
+        {children}
         <Toaster />
       </body>
     </html>
